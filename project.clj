279e413c5dc132217b9e28eb979458f156983242
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-12-19 13:45:03 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,22 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.11.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-time "0.15.1"]
                  [ring "1.4.0"]
-                 [org.apache.santuario/xmlsec "2.0.4"]
-                 [compojure "1.5.0"]
+                 [org.apache.santuario/xmlsec "2.1.2"]
+                 [compojure "1.6.1"]
                  [org.opensaml/opensaml "2.6.4"]
-                 [org.clojure/data.xml "0.0.7"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/data.zip "0.1.1"]
-                 [org.vlacs/helmsman "1.0.0-alpha5"]]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.codec "0.1.1"]
+                 [org.clojure/data.zip "0.1.2"]
+                 [org.vlacs/helmsman "1.0.0"]]
   :pedantic :warn
   :profiles {:dev {:source-paths ["dev" "test"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                  [org.clojure/tools.nrepl "0.2.3"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [org.clojure/tools.nrepl "0.2.13"]
                                   [hiccup "1.0.5"]
-                                  [http-kit "2.1.18"]]}}
+                                  [http-kit "2.3.0"]]}}
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :sign-releases false
                               :username :env
